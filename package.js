332708e326ef832e976d56735039136bd5204bdb
Manuel Schoebel <mail@manuelschoebel.de>
2014-09-27 15:38:34 +0200

    init

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: "manuelschoebel:cheerio",
+  summary: "cheerio wrapped for meteor",
+  git: "https://github.com/DerMambo/cheerio.git",
+  version: "0.0.1"
+});
+
+Npm.depends({cheerio: "0.17.0"});
+
+Package.onUse(function(api){
+  api.versionsFrom("0.9.0");
+
+  api.export && api.export('cheerio');
+  api.addFiles('cheerio.js', 'server');
+
+});
